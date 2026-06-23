0c1e6aca17e8c7a0bb5a7b53ed429046c1dd14cf
sekongger <93640496+sekongger@users.noreply.github.com>
2026-06-23 22:19:42 +0800

    Upload public snapshot batch 132

---
+++ modules/openspg/server/infra/dao/src/main/java/com/antgroup/openspg/server/infra/dao/dataobject/ReleaseDO.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2023 OpenSPG Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied.
+ */
+
+package com.antgroup.openspg.server.infra.dao.dataobject;
+
+import java.util.Date;
+
+public class ReleaseDO {
+  private Long id;
+
+  private Date gmtCreate;
+
+  private Date gmtModified;
+
+  private Long projectId;
+
+  private Integer version;
+
+  private String userNo;
+
+  private String status;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Date getGmtCreate() {
+    return gmtCreate;
+  }
+
+  public void setGmtCreate(Date gmtCreate) {
+    this.gmtCreate = gmtCreate;
+  }
+
+  public Date getGmtModified() {
+    return gmtModified;
+  }
+
+  public void setGmtModified(Date gmtModified) {
+    this.gmtModified = gmtModified;
+  }
+
+  public Long getProjectId() {
+    return projectId;
+  }
+
+  public void setProjectId(Long projectId) {
+    this.projectId = projectId;
+  }
+
+  public Integer getVersion() {
+    return version;
+  }
+
+  public void setVersion(Integer version) {
+    this.version = version;
+  }
+
+  public String getUserNo() {
+    return userNo;
+  }
+
+  public void setUserNo(String userNo) {
+    this.userNo = userNo == null ? null : userNo.trim();
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status == null ? null : status.trim();
+  }
+}
